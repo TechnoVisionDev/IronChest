a586867a977f8089f36e7a6725a2427de23a05be
Modder <60491189+tap6386@users.noreply.github.com>
2020-08-19 13:03:37 -0700

    Finish DirtChest 9000!

--- src/main/java/com/technovision/ironchest/blocks/blockentities/DirtChestBlockEntity.java
+++ src/main/java/com/technovision/ironchest/blocks/blockentities/DirtChestBlockEntity.java
@@ -4,15 +4,28 @@ import com.technovision.ironchest.blocks.ExtraChestTypes;
 import com.technovision.ironchest.registry.ModBlockEntityType;
 import com.technovision.ironchest.registry.ModScreenHandlerType;
 import com.technovision.ironchest.screenhandlers.ExtraChestScreenHandler;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.*;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Language;
 
 public class DirtChestBlockEntity extends GenericIronChestBlockEntity {
 
+    private static final ItemStack guideBook = new ItemStack(Items.WRITTEN_BOOK);
+    private static boolean bookDataCreated = false;
+
     public DirtChestBlockEntity() {
         super(ModBlockEntityType.DIRT_CHEST, ExtraChestTypes.DIRT);
+        if (!bookDataCreated) {
+            createBookData();
+        }
+        this.setStack(0, guideBook.copy());
     }
 
     @Override
@@ -25,4 +38,27 @@ public class DirtChestBlockEntity extends GenericIronChestBlockEntity {
         return new ExtraChestScreenHandler(ModScreenHandlerType.DIRT_CHEST, type, syncId, inventory, ScreenHandlerContext.create(world, pos));
     }
 
+    private static void createBookData() {
+        CompoundTag tag = guideBook.getOrCreateTag();
+
+        tag.putString("title", Language.getInstance().get("book.ironchest.guidebook.title"));
+        tag.putString("author", "TechnoVision");
+
+        ListTag pages = new ListTag();
+        pages.add(StringTag.of(getPage("book.ironchest.guidebook.page1")));
+        pages.add(StringTag.of(getPage("book.ironchest.guidebook.page2")));
+        pages.add(StringTag.of(getPage("book.ironchest.guidebook.page3")));
+        pages.add(StringTag.of(getPage("book.ironchest.guidebook.page4")));
+        pages.add(StringTag.of(getPage("book.ironchest.guidebook.page5")));
+        tag.put("pages", pages);
+
+        guideBook.setTag(tag);
+        bookDataCreated = true;
+    }
+
+    private static String getPage(String key) {
+        String text = Language.getInstance().get(key);
+        return String.format("\"%s\"", text);
+    }
 }
+
