8c0cc2b3434afc54481833fff2a5621770aa0c36
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-12-04 13:43:45 +1100

    Remove unused enum properties in ExtraChestTypes
    I've also changed both the values of the WOOD chest type and the default case for getScreenHandlerType to more closely match the vanilla chest.

--- src/main/java/com/technovision/ironchest/blocks/ExtraChestTypes.java
+++ src/main/java/com/technovision/ironchest/blocks/ExtraChestTypes.java
@@ -17,33 +17,25 @@ import net.minecraft.util.Identifier;
 
 public enum ExtraChestTypes {
 
-    IRON(54, 9, 184, 222, new Identifier(IronChests.MOD_ID, "entity/chest/iron_chest"), 256, 256),
-    GOLD(81, 9, 184, 276, new Identifier(IronChests.MOD_ID, "entity/chest/gold_chest"), 256, 276),
-    DIAMOND(108, 12, 238, 276, new Identifier(IronChests.MOD_ID, "entity/chest/diamond_chest"), 256, 276),
-    COPPER(45, 9, 184, 204, new Identifier(IronChests.MOD_ID, "entity/chest/copper_chest"), 256, 256),
-    SILVER(72, 9, 184, 258, new Identifier(IronChests.MOD_ID, "entity/chest/silver_chest"), 256, 276),
-    CRYSTAL(108, 12, 238, 276, new Identifier(IronChests.MOD_ID, "entity/chest/crystal_chest"), 256, 276),
-    OBSIDIAN(108, 12, 238, 276, new Identifier(IronChests.MOD_ID,"entity/chest/obsidian_chest"), 256, 276),
-    DIRT(1, 1, 184, 184, new Identifier(IronChests.MOD_ID, "entity/chest/dirt_chest"), 256, 256),
-    HOLIDAY(27, 9, 0, 0, new Identifier("entity/chest/christmas"), 0, 0),
-    WOOD(0, 0, 0, 0, new Identifier(IronChests.MOD_ID, ("entity/chest/dirt_chest")), 0, 0);
+    IRON(54, 9, new Identifier(IronChests.MOD_ID, "entity/chest/iron_chest")),
+    GOLD(81, 9, new Identifier(IronChests.MOD_ID, "entity/chest/gold_chest")),
+    DIAMOND(108, 12, new Identifier(IronChests.MOD_ID, "entity/chest/diamond_chest")),
+    COPPER(45, 9, new Identifier(IronChests.MOD_ID, "entity/chest/copper_chest")),
+    SILVER(72, 9, new Identifier(IronChests.MOD_ID, "entity/chest/silver_chest")),
+    CRYSTAL(108, 12, new Identifier(IronChests.MOD_ID, "entity/chest/crystal_chest")),
+    OBSIDIAN(108, 12, new Identifier(IronChests.MOD_ID, "entity/chest/obsidian_chest")),
+    DIRT(1, 1, new Identifier(IronChests.MOD_ID, "entity/chest/dirt_chest")),
+    HOLIDAY(27, 9, new Identifier("entity/chest/christmas")),
+    WOOD(27, 9, new Identifier("entity/chest/normal"));
 
     public final int size;
     public final int rowLength;
-    public final int xSize;
-    public final int ySize;
     public final Identifier texture;
-    public final int textureXSize;
-    public final int textureYSize;
 
-    ExtraChestTypes(int size, int rowLength, int xSize, int ySize, Identifier texture, int textureXSize, int textureYSize) {
+    ExtraChestTypes(int size, int rowLength, Identifier texture) {
         this.size = size;
         this.rowLength = rowLength;
-        this.xSize = xSize;
-        this.ySize = ySize;
         this.texture = texture;
-        this.textureXSize = textureXSize;
-        this.textureYSize = textureYSize;
     }
 
     public int getRowCount() {
@@ -101,7 +93,7 @@ public enum ExtraChestTypes {
                 return new ChestBlockEntity();
         }
     }
-    
+
     public ScreenHandlerType<ExtraChestScreenHandler> getScreenHandlerType() {
         switch (this) {
             case IRON:
@@ -123,10 +115,10 @@ public enum ExtraChestTypes {
             case HOLIDAY:
                 return ModScreenHandlerType.HOLIDAY_CHEST;
             default:
-            	return ModScreenHandlerType.IRON_CHEST;
+            	return ModScreenHandlerType.HOLIDAY_CHEST;
         }
     }
-    
+
     public BlockEntityType<? extends ChestBlockEntity> getBlockEntityType() {
         switch (this) {
             case IRON:
